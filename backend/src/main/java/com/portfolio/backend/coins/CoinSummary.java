c681c66d3838720ad0a9cd67d3c49f90aa823b9a
Reio Sedrik <resedr@ttu.ee>
2018-02-27 19:14:37 +0200

    converting json

---
+++ backend/src/main/java/com/portfolio/backend/coins/CoinSummary.java
@@ -0,0 +1,33 @@
+package com.portfolio.backend.coins;
+
+public class CoinSummary {
+
+    private String marketName;
+    private String high;
+    private String low;
+    private String volume;
+    private String last;
+    private String baseVolume;
+    private String timeStamp;
+    private String bid;
+    private String openBuyOrders;
+    private String openSellOrders;
+    private String prevDay;
+    private String created;
+
+
+
+
+//    {"MarketName":"BTC-1ST",
+//            "High":0.00003090,
+//            "Low":0.00002690,
+//            "Volume":1788912.30621303,
+//            "Last":0.00002766,
+//            "BaseVolume":51.06205661,
+//            "TimeStamp":"2018-02-27T12:31:54.56",
+//            "Bid":0.00002766,"Ask":0.00002818,
+//            "OpenBuyOrders":168,
+//            "OpenSellOrders":2716,
+//            "PrevDay":0.00002825,
+//            "Created":"2017-06-06T01:22:35.727"}
+}
