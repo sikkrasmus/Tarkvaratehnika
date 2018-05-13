c4ef473f1b89802b30a335d977a50793ac5037a9
“schmuelstark” <saamuelstarke@gmail.comssh-keygen -t rsa -C saamuelstarke@gmail.comgit config --global color.ui truegit config --global user.name schmuelstarkgit config --global user.email saamuelstarke@gmail.com>
2018-05-13 19:22:23 +0300

    Add portfolio redesign

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -3,50 +3,46 @@
     <navloggedwithouttabs></navloggedwithouttabs>
     <v-container grid-list-md>
       <v-flex 12>
-        <br>
-        <v-btn color="primary" dark @click.native.stop="dialog = true" style="width: 100%; margin: 0px">Add portfolio
-        </v-btn>
-        <br><br><br><br>
-        <v-dialog v-model="dialog" max-width="400">
-          <v-card>
-            <v-card-title class="headline">Add new portfolio</v-card-title>
-            <v-container>
-              <v-form @submit.prevent="addPortf">
-                <v-text-field
-                  label="Portfolio Name"
-                  v-model="info.portfolioName"
-                  required
-                ></v-text-field>
-                <v-text-field
-                  label="Portfolio Description"
-                  v-model="info.description"
-                  required
-                ></v-text-field>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="green darken-1" flat="flat" @click.native="dialog = false">Cancel</v-btn>
-                  <v-btn color="green darken-1" flat="flat" type="=submit">Add</v-btn>
-                </v-card-actions>
-              </v-form>
-            </v-container>
-          </v-card>
-        </v-dialog>
+        <v-card style="margin-top: 20px; background: linear-gradient(to bottom, #396afc, #2948ff);" color="blue-grey darken-2" class="white--text">
+          <v-card-title primary-title>
+            <div class="headline">
+              Add new portfolio
+              <v-text-field
+                color="white"
+                label="Portfolio Name"
+                v-model="info.portfolioName"
+                required
+                dark
+              ></v-text-field>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              label="Portfolio Description"
+              color="white"
+              v-model="info.description"
+              dark
+            ></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn flat dark large v-on:click="addPortf">Add</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
       <v-layout row wrap>
-        <v-flex 12>
-          <v-card style="margin-top: 20px" color="blue-grey darken-2" class="white--text"
-                  v-for="(portfolio, id) in this.portfolios" :key="value">
-            <v-card-title primary-title>
-              <div class="headline">{{ portfolio[0] }}</div>
-            </v-card-title>
-            <v-card-text>
-              {{ portfolio[1] }}
-            </v-card-text>
-            <v-card-actions>
-              <v-btn flat dark v-on:click="delPortfolio(id)">Delete</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
+          <v-flex lg6 xs12 v-for="(portfolio, id) in this.portfolios" :key="value">
+            <v-card style="margin-top: 20px; background: linear-gradient(to bottom, #00c6ff, #0072ff);" color="blue-grey darken-2" class="white--text">
+              <v-card-title primary-title>
+                <div class="headline">{{ portfolio[0] }}</div>
+              </v-card-title>
+              <v-card-text>
+                {{ portfolio[1] }}
+              </v-card-text>
+              <v-card-actions>
+                <v-btn flat dark v-on:click="delPortfolio(id)">Delete</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
       </v-layout>
     </v-container>
   </v-app>
@@ -103,7 +99,6 @@
           location.reload()
         })
       },
-
       addPortf() {
         this.info.email = this.$store.state.username;
         this.addPortfolio(this.info).then(() => {
